094fc74bdf727b0d78ad005e2c34ad55d786804d
John <ashton.basile@gmail.com>
2023-11-20 16:13:50 +0000

    added further explanation of pointers, references, the depointer operator, and the dereference operator

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -23,9 +23,13 @@ int main()
     Player* ptrToJohn; // Declaring a new variable called ptrToJohn as a pointer of Type Player. 
     // As a pointer of Type player, it will be able to hold (as it's value), but won't yet hold, an address in
     // memory to a Player object
+    // Player* can be explained. The * means that it is a pointer holding a memory address 
+    // and the type of object held at that memory address is a Player object.
 
     ptrToJohn = &john; // Assigning the value of ptrToJohn equal to a reference to the john object
     // this effectively stores the address in memory of the john object as the value of the ptrToJohn variable.
+    // &john shows use of the "Address-of Operator" which I like to call the "Depointer Operator" because it 
+    // returns the address (thus giving us the address) which is the opposite of what a pointer does, in that pointers store addresses
 
     cout << john.hp << endl; // 100
     
@@ -35,6 +39,18 @@ int main()
 
     cout << john.hp << endl; // 67
 
+    // indirect reference
+    Player& indirectRefToJohn = *ptrToJohn; // indirectRefToJohn is another name for john (accessed indirectly through ptrToJohn)
+    // indirectRefToJohn is a reference to an object of type Player
+    // the * operator here is the "Dereference Operator". The Dereference Operator (also called the "Indirection Operator") gets the
+    // reference (which is a direct-link to the value) of a pointer
+
+    cout << indirectRefToJohn.name << endl; // John
+
+    // direct reference
+    Player& directRefToJohn = john; // directRefToJohn is yet another name for john (accessed directly)
+
+    cout << directRefToJohn.name << endl; // John
 
     return 0;
 }
